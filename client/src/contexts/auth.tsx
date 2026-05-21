a43612098871cb2b676176a3bf025c0159eac6fc
RaviAnand Mohabir <moravrav@gmail.com>
2026-05-21 10:23:00 +0200

    feat: :passport_control: handle unauthorized in auth context for me and redirect to login

--- client/src/contexts/auth.tsx
+++ client/src/contexts/auth.tsx
@@ -1,6 +1,8 @@
 import type { Dispatch, ReactNode, SetStateAction } from 'react'
 import { createContext, useContext, useEffect, useState } from 'react'
+import { useLocation, useNavigate, useRouter } from '@tanstack/react-router'
 
+import { AxiosError } from 'axios'
 import type { User } from '#/lib/types/user'
 import { meQuery } from '#/lib/queries/user'
 import { useQuery } from '@tanstack/react-query'
@@ -18,11 +20,19 @@ const AuthContext = createContext<AuthContext>({
 })
 
 export function AuthContextProvider({ children }: { children: ReactNode }) {
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+
   const [user, setUser] = useState<User | null>(null)
 
-  const { data, isLoading } = useQuery({
-    ...meQuery(),
-  })
+  const { data, error, isLoading } = useQuery(meQuery())
+
+  useEffect(() => {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401 && pathname !== '/')
+        navigate({ to: '/' })
+    }
+  }, [error])
 
   useEffect(() => {
     if (data) setUser(data)
